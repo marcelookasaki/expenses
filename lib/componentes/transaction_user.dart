f9df2dd4776f89f3fb3bb0901286991821091afb
marcelookasaki <marcelo.okasaki@gmail.com>
2022-06-28 11:57:52 -0300

    Refatoração dos componentes

---
+++ lib/componentes/transaction_user.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'transaction_form.dart';
+import 'transaction_list.dart';
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({Key? key}) : super(key: key);
+
+  @override
+  State<TransactionUser> createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 520.54,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.12,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(_transactions),
+        TransactionForm()
+      ],
+    )
+  }
+}
